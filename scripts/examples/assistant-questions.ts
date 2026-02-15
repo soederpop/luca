2db0c6ee36cb3992ccf7883e6181518559b3f513
soederpop <jonathan.soeder@gmail.com>
2026-02-14 22:38:43 -0600

    example of using the assistant, need to improve the prompt and add more docs to see how it performs

---
+++ scripts/examples/assistant-questions.ts
@@ -0,0 +1,12 @@
+import container from '@/agi'
+
+const assistant = container.feature('assistant', {
+  folder: 'assistants/example',
+  appendPrompt: 'Always be brief. Search your internal docs to make sure you have all the info you need.',
+	prependPrompt: 'You are speaking to a gentleman named Jon aka Soederpop aka J-Money.',
+  model: 'gpt-4.1',
+})
+
+const secretAnswer = await assistant.ask('Research your internal docs before answering, what is the best city on earth?  Tell me that as well as the current time.  Make sure to greet me by name.')
+
+console.log(container.ui.markdown(secretAnswer))
